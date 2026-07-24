266d23e520fd5f1e6ddd0362896352d202704932
purity0km-droid <purity0km@gmail.com>
2026-07-24 14:43:22 +0100

    1枚保存用レイアウト調整

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -10,58 +10,36 @@ export default function Preview({
   exportPreviewRef,
   exportMode = false,
 }) {
+
+  const exportClass =
+    exportMode
+      ? relations.length === 1
+        ? "export-mode-single"
+        : "export-mode"
+      : "";
+
   return (
     <div className="preview">
 
-    <div
-      ref={exportMode ? exportPreviewRef : previewRef}
-      className={`paper pattern-${page.backgroundPattern} ${
-        exportMode ? "export-mode" : ""
-      }`}
-      style={{
-        "--paper-color": page.backgroundColor,
-        color: page.textColor,
-        fontFamily: page.fontFamily,
-      }}
-    >
+      <div
+        ref={exportMode ? exportPreviewRef : previewRef}
+        className={`paper pattern-${page.backgroundPattern} ${exportClass}`}
+        style={{
+          "--paper-color": page.backgroundColor,
+          color: page.textColor,
+          fontFamily: page.fontFamily,
+        }}
+      >
 
         {page.showTitle && (
           <h1>{page.title || "タイトル"}</h1>
         )}
 
-        {/* 1枚 */}
-        {relations.length === 1 && (
-          <div className="cardGrid one">
+        <div className="paperContent">
 
-            <RelationCard
-              relation={relations[0]}
-              layout="large"
-              updateRelation={updateRelation}
-            />
-
-          </div>
-        )}
-
-        {/* 2枚 */}
-        {relations.length === 2 && (
-          <div className="cardGrid two">
-
-            {relations.map((relation) => (
-              <RelationCard
-                key={relation.id}
-                relation={relation}
-                layout="medium"
-                updateRelation={updateRelation}
-              />
-            ))}
-
-          </div>
-        )}
-
-        {/* 3枚 */}
-        {relations.length === 3 && (
-          <>
-            <div className="cardGrid threeTop">
+          {/* 1枚 */}
+          {relations.length === 1 && (
+            <div className="cardGrid one">
 
               <RelationCard
                 relation={relations[0]}
@@ -70,40 +48,72 @@ export default function Preview({
               />
 
             </div>
+          )}
 
-            <div className="cardGrid threeBottom">
+          {/* 2枚 */}
+          {relations.length === 2 && (
+            <div className="cardGrid two">
 
-              <RelationCard
-                relation={relations[1]}
-                layout="small"
-                updateRelation={updateRelation}
-              />
-
-              <RelationCard
-                relation={relations[2]}
-                layout="small"
-                updateRelation={updateRelation}
-              />
+              {relations.map((relation) => (
+                <RelationCard
+                  key={relation.id}
+                  relation={relation}
+                  layout="medium"
+                  updateRelation={updateRelation}
+                />
+              ))}
 
             </div>
-          </>
-        )}
+          )}
 
-        {/* 4枚 */}
-        {relations.length === 4 && (
-          <div className="cardGrid four">
+          {/* 3枚 */}
+          {relations.length === 3 && (
+            <>
+              <div className="cardGrid threeTop">
 
-            {relations.map((relation) => (
-              <RelationCard
-                key={relation.id}
-                relation={relation}
-                layout="small"
-                updateRelation={updateRelation}
-              />
-            ))}
+                <RelationCard
+                  relation={relations[0]}
+                  layout="large"
+                  updateRelation={updateRelation}
+                />
 
-          </div>
-        )}
+              </div>
+
+              <div className="cardGrid threeBottom">
+
+                <RelationCard
+                  relation={relations[1]}
+                  layout="small"
+                  updateRelation={updateRelation}
+                />
+
+                <RelationCard
+                  relation={relations[2]}
+                  layout="small"
+                  updateRelation={updateRelation}
+                />
+
+              </div>
+            </>
+          )}
+
+          {/* 4枚 */}
+          {relations.length === 4 && (
+            <div className="cardGrid four">
+
+              {relations.map((relation) => (
+                <RelationCard
+                  key={relation.id}
+                  relation={relation}
+                  layout="small"
+                  updateRelation={updateRelation}
+                />
+              ))}
+
+            </div>
+          )}
+
+        </div>
 
       </div>
 
